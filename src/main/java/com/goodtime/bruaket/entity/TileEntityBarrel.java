0d08626e105fae56b71305b372e1c5402f213e9a
DESKTOP-2OPAG06\56362 <563622979@qq.com>
2022-03-03 20:54:12 +0800

    Barrel cant pull any item from other container now. Add dispense function.

--- src/main/java/com/goodtime/bruaket/entity/TileEntityBarrel.java
+++ src/main/java/com/goodtime/bruaket/entity/TileEntityBarrel.java
@@ -2,15 +2,24 @@ package com.goodtime.bruaket.entity;
 
 
 import com.goodtime.bruaket.blocks.Barrel;
+import net.minecraft.block.BlockSourceImpl;
+import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
+import net.minecraft.dispenser.IBehaviorDispenseItem;
+import net.minecraft.dispenser.PositionImpl;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.IHopper;
+import net.minecraft.tileentity.TileEntityDispenser;
 import net.minecraft.tileentity.TileEntityHopper;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 
+import java.util.List;
 import java.util.UUID;
 
 public class TileEntityBarrel extends TileEntityHopper {
@@ -88,8 +97,13 @@ public class TileEntityBarrel extends TileEntityHopper {
                     flag = this.transferItemsOut();
                 }
 
-                if (flag)
-                {
+                //如果漏斗没满，接收投掷物
+                if (!this.isFull()) {
+                    flag = pullItems(this) || flag;
+                }
+
+                //8tick传输一个物品
+                if (flag) {
                     this.setTransferCooldown(8);
                     this.markDirty();
                     return true;
@@ -103,74 +117,85 @@ public class TileEntityBarrel extends TileEntityHopper {
         }
     }
 
+    public static boolean pullItems(IHopper hopper) {
+       /* Boolean ret = net.minecraftforge.items.VanillaInventoryCodeHooks.extractHook(hopper);
+        if (ret != null) return ret;*/
+
+        List<EntityItem> items = getCaptureItems(hopper.getWorld(), hopper.getXPos(), hopper.getYPos(), hopper.getZPos());
+        if(!items.isEmpty()){
+            EntityItem entityitem = items.get(0);
+            return putDropInInventoryAllSlots((IInventory)null, hopper, entityitem);
+        }
+        return false;
+    }
+
     //传输物品，如果底部方块没有储存功能就传输失败
-    private boolean transferItemsOut()
-    {
+    private boolean transferItemsOut() {
         if (net.minecraftforge.items.VanillaInventoryCodeHooks.insertHook(this)) { return true; }
         IInventory iinventory = this.getInventoryForBarrelTransfer();
 
-        if (iinventory == null)
-        {
-            return false;
+        //如果检测不到有存储功能的方块，就把合成产出丢出来
+        if (iinventory == null) {
+
+            for (int i = 0; i < this.getSizeInventory() - 1; i++) {
+                this.setInventorySlotContents(i, ItemStack.EMPTY);
+            }
+
+            dispense(this,new ItemStack(Items.APPLE));
+            return true;
         }
-        else
-        {
+        else {
             EnumFacing enumfacing = Barrel.getFacing(this.getBlockMetadata()).getOpposite();
 
-            if (this.isInventoryFull(iinventory, enumfacing))
-            {
+            if (this.isInventoryFull(iinventory, enumfacing)) {
                 return false;
             }
-            else
-            {
-                for (int i = 0; i < this.getSizeInventory(); ++i)
-                {
-                    if (!this.getStackInSlot(i).isEmpty())
-                    {
+            else {
+                for (int i = 0; i < this.getSizeInventory()-1; ++i) {
+                    if (!this.getStackInSlot(i).isEmpty()) {
                         ItemStack itemstack = this.getStackInSlot(i).copy();
                         ItemStack itemstack1 = putStackInInventoryAllSlots(this, iinventory, this.decrStackSize(i, 1), enumfacing);
 
-                        if (itemstack1.isEmpty())
-                        {
+                        if (itemstack1.isEmpty()) {
                             iinventory.markDirty();
                             return true;
                         }
-
                         this.setInventorySlotContents(i, itemstack);
                     }
                 }
-
                 return false;
             }
         }
     }
 
+    //冒尖冒尖！
+    private void dispense(IHopper barrel, ItemStack itemstack){
+        double x = barrel.getXPos();
+        double y = barrel.getYPos() - 1;
+        double z = barrel.getZPos();
+        BehaviorDefaultDispenseItem.doDispense(barrel.getWorld(), itemstack, 3, EnumFacing.DOWN, new PositionImpl(x, y, z));
+    }
+
     //获取桶底部的方块的库存（inventory），如果没有库存就返回null
-    private IInventory getInventoryForBarrelTransfer()
-    {
+    private IInventory getInventoryForBarrelTransfer() {
         EnumFacing enumfacing = Barrel.getFacing(this.getBlockMetadata());
         return getInventoryAtPosition(this.getWorld(), this.getXPos() + (double)enumfacing.getFrontOffsetX(), this.getYPos() + (double)enumfacing.getFrontOffsetY(), this.getZPos() + (double)enumfacing.getFrontOffsetZ());
     }
 
-    private boolean isOnTransferCooldown()
-    {
+    private boolean isOnTransferCooldown() {
         return this.transferCooldown > 0;
     }
 
-    public boolean mayTransfer()
-    {
+    public boolean mayTransfer() {
         return this.transferCooldown > 8;
     }
 
-    public void setTransferCooldown(int ticks)
-    {
+    public void setTransferCooldown(int ticks) {
         this.transferCooldown = ticks;
     }
 
-    private boolean isInventoryFull(IInventory inventoryIn, EnumFacing side)
-    {
-        if (inventoryIn instanceof ISidedInventory)
-        {
+    private boolean isInventoryFull(IInventory inventoryIn, EnumFacing side) {
+        if (inventoryIn instanceof ISidedInventory) {
             ISidedInventory isidedinventory = (ISidedInventory)inventoryIn;
             int[] aint = isidedinventory.getSlotsForFace(side);
 
@@ -183,17 +208,13 @@ public class TileEntityBarrel extends TileEntityHopper {
                     return false;
                 }
             }
-        }
-        else
-        {
+        } else {
             int i = inventoryIn.getSizeInventory();
 
-            for (int j = 0; j < i; ++j)
-            {
+            for (int j = 0; j < i; ++j) {
                 ItemStack itemstack = inventoryIn.getStackInSlot(j);
 
-                if (itemstack.isEmpty() || itemstack.getCount() != itemstack.getMaxStackSize())
-                {
+                if (itemstack.isEmpty() || itemstack.getCount() != itemstack.getMaxStackSize()) {
                     return false;
                 }
             }
@@ -202,12 +223,9 @@ public class TileEntityBarrel extends TileEntityHopper {
         return true;
     }
 
-    private boolean isInventoryEmpty()
-    {
-        for (ItemStack itemstack : this.inventory)
-        {
-            if (!itemstack.isEmpty())
-            {
+    private boolean isInventoryEmpty() {
+        for (ItemStack itemstack : this.inventory) {
+            if (!itemstack.isEmpty()) {
                 return false;
             }
         }
@@ -227,25 +245,21 @@ public class TileEntityBarrel extends TileEntityHopper {
         return true;
     }
 
-    public int getInventoryStackLimit()
-    {
+    public int getInventoryStackLimit() {
         return 64;
     }
 
-    protected NonNullList<ItemStack> getItems()
-    {
+    protected NonNullList<ItemStack> getItems() {
         return this.inventory;
     }
 
 
-    public int getSizeInventory()
-    {
+    public int getSizeInventory() {
         return this.inventory.size();
     }
 
 
-    public void setOwner(UUID owner)
-    {
+    public void setOwner(UUID owner) {
         this.owner = owner;
     }
 
